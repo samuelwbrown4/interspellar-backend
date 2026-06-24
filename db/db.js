cb18a6104649f2ddc8bae1e8709d6b86ddf12529
Samuel Brown <samuelwbrown4@gmail.com>
2026-06-24 09:35:50 -0400

    separation of db and prod dbs. introduction of node_env variables

--- db/db.js
+++ db/db.js
@@ -2,7 +2,10 @@ const path = require('path')
 const Database = require('better-sqlite3')
 const {initializeSchema} = require('./schema.js')
 
-const db = new Database(path.join(__dirname, 'planetary-puzzles.db'));
+const dbPath = process.env.NODE_ENV === 'production'
+    ? 'prod.db'
+    : 'dev.db'
+const db = new Database(path.join(__dirname, dbPath));
 db.exec('PRAGMA foreign_keys = ON');
 initializeSchema(db)
 
